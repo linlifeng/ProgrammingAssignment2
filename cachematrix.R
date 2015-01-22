e1e850a2d3188e26d51ab6fee4de54b86b24c804
linlifeng <bioinformaticsscientist@gmail.com>
2015-01-22 14:45:54 -0600

    updated answer
    added comment for each function

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions serve to cache the result of a matrix inversion.
+## If a matrix has been inverted before, this will save time and resources 
+## by recalling the stored value. 
 
-## Write a short comment describing this function
+## makeCachedMatrix take an matrix and precalculate the inverted matrix for later use.
+## input is a matrix
+## return a vector that is a list of functions
+##    set the value of the matrix
+##    get the value of the matrix
+##    set the value of the inverted matrix
+##    get the value of the inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
         I <- NULL
@@ -13,11 +20,12 @@ makeCacheMatrix <- function(x = matrix()) {
         setInv <- function(Inv) I <<- Inv
         getInv <- function() I
         list(set = set, get = get, setInv = setInv, getInv = getInv)
-        
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes in the input from the vector created by makeCacheMatrix and calculate the invert matrix.
+## if the inverted matrix has been calculated, it returns the stored value
+## otherwise, it calculate the inverted matrix and assign the value to be stored.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
