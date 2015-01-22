27677659357bbc3c98f70461c92e9a6646ccca2e
linlifeng <bioinformaticsscientist@gmail.com>
2015-01-22 16:56:09 -0600

    comment for each function
    moved comments to their respective positions in each function

--- cachematrix.R
+++ cachematrix.R
@@ -2,15 +2,15 @@
 ## If a matrix has been inverted before, this will save time and resources 
 ## by recalling the stored value. 
 
-## makeCachedMatrix take an matrix and precalculate the inverted matrix for later use.
-## input is a matrix
-## return a vector that is a list of functions
-##    set the value of the matrix
-##    get the value of the matrix
-##    set the value of the inverted matrix
-##    get the value of the inverted matrix
-
 makeCacheMatrix <- function(x = matrix()) {
+        # makeCachedMatrix take an matrix and precalculate the inverted 
+        # matrix for later use.
+        # input is a matrix
+        # return a vector that is a list of functions
+        #    set the value of the matrix
+        #    get the value of the matrix
+        #    set the value of the inverted matrix
+        #    get the value of the inverted matrix
         I <- NULL
         set <- function(y){
                 x <<- y
@@ -23,12 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve takes in the input from the vector created by makeCacheMatrix and calculate the invert matrix.
-## if the inverted matrix has been calculated, it returns the stored value
-## otherwise, it calculate the inverted matrix and assign the value to be stored.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # cacheSolve takes in the input from the vector created by makeCacheMatrix 
+        # and calculate the invert matrix.
+        # If the inverted matrix has been calculated, it returns the stored value
+        # otherwise, it calculate the inverted matrix and assign the value to be stored.
+        # Return a matrix that is the inverse of 'x'
         I <- x$getInv()
         if(!is.null(I)) {
                 message("getting cached data")
